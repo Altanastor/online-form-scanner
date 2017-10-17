492aa66477ddc822e3f9b224e9601d41db737199
Josh Kraan <joshua.kraan.jk@gmail.com>
2017-10-16 19:59:32 -0700

    Initial Commit

---
+++ server.R
@@ -0,0 +1,148 @@
+library(shiny)
+library(shinyBS)
+library(shinyjs)
+library(shinydashboard)
+library(xlsx)
+
+shinyServer(function(input, output, session) {
+  
+  options(shiny.maxRequestSize = 100*1024^2)
+  
+  downloadInputs = c("downloadFile", "downloadCSV", "downloadImages")
+  
+  for (name in downloadInputs) {
+    shinyjs::disable(name)
+  }
+  
+  tempDirectory = paste0(tempdir(), "/", format(Sys.time(), "%Y%m%d%H%M%S"), sample(0:100, 1))
+  
+  shinyjs::addClass("uploadBox", "opacity")
+  shinyjs::addClass("downloadBox", "opacity")
+  
+  observe({
+    if (input$formType != "Please Select a Form Type") {
+      shinyjs::addClass("formTypeBox", "opacity")
+      shinyjs::removeClass("uploadBox", "opacity")
+    }
+  })
+  
+  shinyjs::hide("processResult")
+  
+  output$processResult = renderUI({
+    helpText(icon("spinner", "fa-spin"), "Scanning forms...")
+  })
+  
+  formType = function() {
+    switch(input$formType,
+           "hybrid" = return("/home/shiny/formscanner/Hybrid_v3.2.xtmpl"),
+           "numeric" = return("/home/shiny/formscanner/Numeric_v3.2.xtmpl"),
+           "multi" = return("/home/shiny/formscanner/MC6_75_v3.2.xtmpl"))
+  }
+  
+  excelType = function() {
+    switch(input$formType,
+           "hybrid" = return("ResultsHybridv3.2.xlsm"),
+           "numeric" = return("ResultsNumericv3.2.xlsm"),
+           "multi" = return("ResultsMCv3.2.xlsm"))
+  }
+  
+  
+  # TODO: Change indicator depending on step of process.
+  observeEvent(input$fileUpload, {
+    
+    #print(formType())
+    
+    shinyjs::show("processResult")
+    
+    # rv$fileState = "uploaded"
+    
+    if(dir.exists(tempDirectory)) {
+      shinyjs::disable("downloadFile")
+      unlink(tempDirectory, recursive = TRUE)
+    }
+    
+    print(tempDirectory)
+    
+    inputFile = input$fileUpload
+    dir.create(tempDirectory)
+    setwd(tempDirectory)
+    dir.create("images")
+    if (tools::file_ext(input$fileUpload$name) == "pdf") {
+      file.copy(inputFile$datapath, "forms.pdf")
+      system("nconvert -xall -dpi 300 -out jpeg -o 'images/form-###.jpeg' forms.pdf")
+    } else {
+      unzip(inputFile$datapath, exdir = "images")
+    }
+    system(paste("/bin/bash formscanner", formType(), "images"))
+    system("mv images/*.csv result.tmp")
+    system("java -jar /home/shiny/formscanner/ProcessCSV2.jar result.tmp results.csv")
+    system(paste0("cp /home/shiny/formscanner/", excelType(), " ."))
+    
+    data = read.csv("results.csv")
+    excelForm = loadWorkbook(excelType())
+    sheets = getSheets(excelForm)
+    addDataFrame(data, sheets[[1]], col.names = FALSE, row.names = FALSE, startRow=2, colStyle = NULL)
+    saveWorkbook(excelForm, "results.xlsm")
+    
+    # output$processResult = renderUI({
+    #   helpText(icon("check"), "Scanning Done!")
+    # })
+    
+    # rv$fileState = "processed"
+    
+    output$processResult = renderUI({
+      helpText(icon("check"), "Scanning Done!")
+    })
+    
+    shinyjs::show("processResult")
+    
+    for (name in downloadInputs) {
+      shinyjs::enable(name)
+    }
+    
+    shinyjs::addClass("uploadBox", "opacity")
+    shinyjs::removeClass("downloadBox", "opacity")
+    # shinyjs::show("downloadBox", anim = TRUE)
+    # shinyjs::hide("uploadBox", anim = TRUE)
+    
+  })
+  
+  observeEvent(input$reset, {
+    shinyjs::runjs("location.reload(true);")
+  })
+  
+  output$downloadFile = downloadHandler(
+    filename = function() {
+      paste0(tools::file_path_sans_ext(input$fileUpload$name), ".xlsm")
+    },
+
+    content = function(file) {
+      file.copy("results.xlsm", file)
+    }
+  )
+  
+  output$downloadCSV = downloadHandler(
+    filename = function() {
+      paste0(tools::file_path_sans_ext(input$fileUpload$name), ".csv")
+    },
+    
+    content = function(file) {
+      file.copy("results.csv", file)
+    }
+  )
+  
+  output$downloadImages = downloadHandler(
+    filename = function() {
+      paste0(tools::file_path_sans_ext(input$fileUpload$name), ".zip")
+    },
+    
+    content = function(file) {
+      system("zip files.zip images/*.jpeg")
+      file.copy("files.zip", file)
+    }
+  )
+  
+  session$onSessionEnded(function() {
+    unlink(tempDirectory, recursive = TRUE)
+  })
+})
---
+++ ui.R
@@ -0,0 +1,104 @@
+library(shiny)
+library(shinyBS)
+library(shinyjs)
+library(shinydashboard)
+
+body = dashboardBody(
+  useShinyjs(),
+  inlineCSS(list(.opacity = "opacity: 0.5; pointer-events: none")),
+  fluidRow(
+    column(8, offset = 2, {
+      div(id = "formTypeBox", 
+          box(
+            title = "1. Select Form Type",
+            status = "primary",
+            solidHeader = TRUE,
+            width = 12,
+            helpText("Please select the form type you are scanning."),
+            selectInput("formType", label = NULL,
+                        c("Please Select a Form Type", 
+                          "Hybrid" = "hybrid", 
+                          "Numeric" = "numeric", 
+                          "Multiple Choice" = "multi"))
+          ))
+    })
+  ), 
+  fluidRow(
+    column(8, offset = 2, {
+      div(id = "uploadBox",
+          box(
+            title = "2. Upload Forms",
+            status = "primary",
+            solidHeader = TRUE,
+            width = 12,
+            helpText("Please upload the forms as one multi-page PDF or a zip file containing JPEG images."),
+            fileInput("fileUpload", label = NULL, 
+                      accept = c(
+                        "application/pdf",
+                        "application/zip",
+                        ".pdf",
+                        ".zip"
+                      )),
+            uiOutput("processResult")
+          ))
+      
+    })
+  ), 
+  fluidRow(
+    column(8, offset = 2, {
+      div(id = "downloadBox",
+          box(
+            title = "3. Download Result",
+            status = "primary",
+            solidHeader = TRUE,
+            width = 12,
+            helpText("Download the result as an Excel file, a CSV file, or download a ZIP file containing seperate images of every form."),
+            downloadButton("downloadFile", "Download Excel File"),
+            downloadButton("downloadCSV", "Download CSV"),
+            downloadButton("downloadImages", "Download Images")
+          ))
+      
+    })
+  ),
+  fluidRow(
+    column(8, offset = 2, align = "center", 
+           actionButton("reset", "Reset", width = "10%", 
+                        icon("refresh"), style = "color: #fff; background-color: #64090F; border-color: #64090F"))
+  ),
+  # Styling look
+  tags$head(tags$style(HTML('
+                            .main-header .logo {
+                                                font-family: "Georgia", Times, "Times New Roman", serif;
+                                                font-weight: bold;
+                                                font-size: 35px;
+                                                text-align: left;
+                            }
+                            .skin-blue .main-header .logo {background-color: #64090F;}
+                            .skin-blue .main-header .logo:hover {background-color: #64090F;}
+                            .skin-blue .main-header .navbar {background-color: #64090F;}
+                            .skin-blue .main-sidebar {background-color: #64090F;}
+                            .box.box-solid.box-primary>.box-header {color: #fff; background: #64090F;}
+                            .box.box-solid.box-primary {
+                                                          border-bottom-color: #64090F;
+                                                          border-left-color: #64090F;
+                                                          border-right-color: #64090F;
+                                                          border-top-color: #64090F;
+                                                          }')))
+)
+
+#Ugly hack to get links into the header
+header = dashboardHeader(title = "RHScan",
+  tags$li(class = "dropdown",
+          tags$a("About", target="_blank", href = "https://google.com")),
+  tags$li(class = "dropdown",
+          tags$a("Download Forms", target="_blank", href = "https://google.com")),
+  tags$li(class = "dropdown",
+          tags$a("FAQs", target="_blank", href = "https://google.com")),
+  tags$li(class = "dropdown",
+          tags$a("Documentation", target="_blank", href = "https://google.com"))
+)
+dashboardPage(
+  header,
+  dashboardSidebar(disable = TRUE),
+  body
+)
